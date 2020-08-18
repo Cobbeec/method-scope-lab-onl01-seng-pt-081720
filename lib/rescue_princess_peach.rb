edc1d7f6c45632033315f0e50c9dd20250acad41
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-18 22:58:17 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,4 +1,5 @@
-
+rescue_princess_peach
+puts status 
 
 def rescue_princess_peach
   status = "rescued"
@@ -6,6 +7,3 @@ def rescue_princess_peach
 end
 
 ## Comment the below back in to see method scope in action!
-
-rescue_princess_peach
-puts status
